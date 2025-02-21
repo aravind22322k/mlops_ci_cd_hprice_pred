6d34a24527c24367feed442f1b56237ba497d71e
aravind22322k <aravindsg072@gmail.com>
2025-02-22 00:06:16 +0530

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -3,20 +3,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
+
 # Load data
 data = pd.read_csv("../data/housing.csv")
 X = data.drop("price", axis=1)
 y = data["price"]
 
+
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
 
 # Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+
 # Save model
 with open("../models/model.pkl", "wb") as f:
     pickle.dump(model, f)
+
 
 print("Model trained and saved!")
