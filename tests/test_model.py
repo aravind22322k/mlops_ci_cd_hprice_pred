0b0eb100110c09792ce2cfdc5b3bd8d33c7b70d9
aravind22322k <aravindsg072@gmail.com>
2025-02-21 23:41:32 +0530

    Create test_model.py

---
+++ tests/test_model.py
@@ -0,0 +1,7 @@
+import pytest
+from scripts.predict import predict_price
+
+def test_predict_price():
+    input_data = [2000, 3]
+    predicted_price = predict_price(input_data)
+    assert isinstance(predicted_price, float), "Prediction should be a float value"
