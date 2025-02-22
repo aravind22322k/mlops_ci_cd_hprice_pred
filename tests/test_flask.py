faaa77901936d9e7a902671835121d79bd1e0b7f
aravind22322k <aravindsg072@gmail.com>
2025-02-22 14:29:45 +0530

    Update test_flask.py

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,20 +1,2 @@
 # tests/test_flask.py
-import pytest
-from app.app import app  # Import your Flask app from app/app.py
 
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-
-def test_predict_endpoint(client):
-    # Test the /predict endpoint
-    response = client.post(
-        "/predict",
-        json={"input": [2000, 3]},  # Example input
-    )
-    assert response.status_code == 200
-    assert "predicted_price" in response.get_json()
