60da8b5da960bbcfbe88bb700e77ade2f6dad4a9
aravind22322k <aravindsg072@gmail.com>
2025-02-22 16:16:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from scripts.predict import predict_price
+
+# Create the Flask app object
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    input_data = data["input"]
+    prediction = predict_price(input_data)
+    return jsonify({"predicted_price": prediction[0]})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
