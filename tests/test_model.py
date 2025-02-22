5b41721b1a58a30b0ba526ed3abe0bbd683221e3
aravind22322k <aravindsg072@gmail.com>
2025-02-22 13:35:40 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,7 @@
 # tests/test_model.py
-from scripts.predict import predict_price  # Import predict_price from scripts/predict.py
+from scripts.predict import (
+    predict_price,  # Import predict_price from scripts/predict.py
+)
 
 
 def test_predict_price():
